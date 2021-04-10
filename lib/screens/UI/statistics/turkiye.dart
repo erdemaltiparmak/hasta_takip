534213d5942e6a0a770e66791ca7c4df43dcbc1b
Erdem Altıparmak <erdem52ap@gmail.com>
2021-04-10 17:55:16 +0300

    case stats

--- lib/screens/UI/statistics/turkiye.dart
+++ lib/screens/UI/statistics/turkiye.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:hasta_takip/models/country_summary.dart';
+import 'package:hasta_takip/services/covid_service.dart';
+
+CovidService covidService = CovidService();
 
 class TurkiyeIstatistik extends StatefulWidget {
   @override
@@ -7,18 +10,48 @@ class TurkiyeIstatistik extends StatefulWidget {
 }
 
 class _TurkiyeIstatistikState extends State<TurkiyeIstatistik> {
+  Future<List<CountrySummaryModel>> summary;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    summary = covidService.getTurkeyStatistics();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-         
-      ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: ListView(
-              scrollDirection: ,
-              children: [Text("data"), Text("data")],
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Güncel Türkiye Vaka Sayıları",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                    GestureDetector(
+                      onTap: null,
+                      child: Icon(
+                        Icons.refresh,
+                        color: Colors.green,
+                      ),
+                    )
+                  ],
+                ),
+                Divider(
+                  color: Colors.green.shade600,
+                  // thickness: 0.8,
+                )
+              ],
             ),
           )
         ],
--- lib/services/covid_service.dart
+++ lib/services/covid_service.dart
@@ -1,26 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import '../models/global_summary.dart';
 import '../models/country_summary.dart';
-import '../models/country.dart';
 
 class CovidService {
-  Future<GlobalSummaryModel> getGlobalSummary() async {
-    final data = await http.Client()
-        .get(Uri.parse("https://api.covid19api.com/summary"));
-
-    if (data.statusCode != 200) throw Exception();
-
-    GlobalSummaryModel summary =
-        new GlobalSummaryModel.fromJson(json.decode(data.body));
-
-    return summary;
-  }
-
-  Future<List<CountrySummaryModel>> getCountrySummary(String slug) async {
+  Future<List<CountrySummaryModel>> getTurkeyStatistics() async {
     final data = await http.Client().get(
-        Uri.parse("https://api.covid19api.com/total/dayone/country/" + slug));
+        Uri.parse("https://api.covid19api.com/total/dayone/country/turkey"));
 
     if (data.statusCode != 200) throw Exception();
 
@@ -29,18 +15,5 @@ class CovidService {
         .toList();
 
     return summaryList;
-  }
-
-  Future<List<CountryModel>> getCountryList() async {
-    final data = await http.Client()
-        .get(Uri.parse("https://api.covid19api.com/countries"));
-
-    if (data.statusCode != 200) throw Exception();
-
-    List<CountryModel> countries = (json.decode(data.body) as List)
-        .map((item) => new CountryModel.fromJson(item))
-        .toList();
-
-    return countries;
   }
 }
